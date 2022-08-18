3d7232ff49fad9a762d7fc3eec53fee423aea6cd
Anna Lee <annakatherinelee@me.com>
2022-08-17 18:19:52 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,7 +8,22 @@ head(MechaCar_mpg)
 ## Perform a linear regression lm()
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
 
+lm(qsec ~ hp,mtcars) ## Determining the slope (hp)
+
 ## Determine the p-value and r-squared value for the linear model summary()
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
 
 ## Save file @annakthrnlee
+
+##### Deliverable 2 #####
+
+## Load csv file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+## Create a total_summary df 
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+## Create a lot_summary df
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep')
+
+## Save file @annakthrnlee 
